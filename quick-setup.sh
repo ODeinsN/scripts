4a2dc9d11bdc16b18c35dd0a3ebc52f0e5eb11c0
Alexander Ruppe <alexander.ruppe@sit.fraunhofer.de>
2024-11-27 14:07:48 +0100

    optimised quich-setup.sh

--- quick-setup.sh
+++ quick-setup.sh
@@ -8,13 +8,10 @@ sudo apt install curl vim git wget zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ./install-vim-plug.sh
 
-$header "Making zsh default"
-chsh -s $(which zsh)
-
 $header "setting up dotfiles"
 git clone --bare https://github.com/odeinsn/dotfiles $HOME/dotfiles
 
-config="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 
 "$config" checkout -f
 "$config" config --local status.showUntrackedFiles no
